fe59e1364b4417a67f16c1415dd8dda0c6a1ed28
dabisi <dabisi@slac.stanford.edu>
2021-04-01 13:34:37 -0700

    Deleted excess comments and made Qt object names more meaningful

--- Derikka/CavityTest_code.py
+++ Derikka/CavityTest_code.py
@@ -11,32 +11,30 @@ class CavityTest(Display):
 	def __init__(self, parent=None, args=None):
 		super(CavityTest, self).__init__(parent=parent, args=args)
 
-		# Parameters
-		self.PV = ''									# current PV
-		self.Test_PV = 'SIOC:SYS0:ML07:AO010'			# PV string
+		# PARAMETERS
+		#current PV
+		self.PV = ''
+		self.Test_PV = 'SIOC:SYS0:ML07:AO010'
 		self.value = caget(self.Test_PV)				
 
 		# Button Connections (send signal and then call a function)
 		self.ui.Qt_pvPushButton.clicked.connect(lambda:self.updatePVOutput())				
 
-		self.ui.Qt_radioButton0.PV = self.Test_PV		
-		self.ui.Qt_radioButton0.value = 0
-		self.ui.Qt_radioButton0.toggled.connect(self.change_PV)
+		self.ui.radioButtonPV0.PV = self.Test_PV		
+		self.ui.radioButtonPV0.value = 0
+		self.ui.radioButtonPV0.toggled.connect(self.change_PV)
 
-		self.ui.Qt_radioButton1.PV = self.Test_PV
-		self.ui.Qt_radioButton1.value = 1
-		self.ui.Qt_radioButton1.toggled.connect(self.change_PV)	
+		self.ui.radioButtonPV1.PV = self.Test_PV
+		self.ui.radioButtonPV1.value = 1
+		self.ui.radioButtonPV1.toggled.connect(self.change_PV)	
 		
 
 	# change pv value with caput command
 	def change_PV(self):
-		# self.sender() identifies object that triggered event (i.e. which radio button was pressed)
 		radioButton = self.sender() 
 		if radioButton.isChecked():
-			caput(radioButton.PV, radioButton.value)					# Use radioButton.PV in place of 'SIOC:SYS0:ML07:AO010'
-			self.ui.Qt_radioOutput.setText("Radio button toggled to PV value of " + str(radioButton.value) )					
-	
-		print("Terminal Text: Radio Button pressed " + str(radioButton.value) )		
+			caput(radioButton.PV, radioButton.value)
+			self.ui.Qt_radioOutput.setText("Radio button toggled to PV value of " + str(radioButton.value) )						
 
 		self.colorChange()
 			
@@ -44,8 +42,6 @@ class CavityTest(Display):
 	def updatePVOutput(self):
 		output = "Button Pushed, PV = " + str(caget(self.Test_PV))		
 		self.ui.Qt_pvOutput.setText(output)
-	
-		print("Terminal Text: " + str(output))
 	
 	# Change the color of something via .setStyleSheet depending on its value
 	def colorChange(self):
@@ -58,8 +54,6 @@ class CavityTest(Display):
 			self.ui.label_CM01.setStyleSheet( str(self.text) + str(self.green) )
 		else:
 			self.ui.label_CM01.setStyleSheet( str(self.text) + str(self.red) )	
-	
-			print("Terminal Text: We're in the colorChange function!")
 			
 	
 	

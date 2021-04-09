e48df63558fd573a0eebdcc9ff00befadc04157c
physics group account <nneveu@slac.stanford.edu>
2021-04-09 10:56:07 -0700

    color changing status bar for individual cavity

--- Derikka/IndividualCavity_code.py
+++ Derikka/IndividualCavity_code.py
@@ -1,9 +1,9 @@
 from pydm import Display
 from PyQt5 import QtGui, QtCore, QtWidgets #, QPainter (Why this not work?) 
-from PyQt5.QtGui import QPainter, QColor, QBrush
+from PyQt5.QtGui import QPainter, QColor, QBrush, QPen
+from PyQt5.QtCore import Qt
 import epics
 from epics import caget, caput
-
 
 
 class IndividualCavity(Display):
@@ -30,31 +30,26 @@ class IndividualCavity(Display):
 		self.ui.cmON.value = 1
 		self.ui.cmON.toggled.connect(self.change_PV)
 
-		
+#		self.statusBarColor()
+
+	
 	# change pv value with caput command
 	def change_PV(self):
 		radioButton = self.sender() 
 		if radioButton.isChecked():
-			caput(radioButton.PV, radioButton.value)
-			
+			caput(radioButton.PV, radioButton.value)	
 		self.statusBarColor()
 		
 
 # Change the color of status bar via .setStyleSheet
 	def statusBarColor(self):
 		self.green = "background-color: rgb(153, 255, 148);\n"
-		Green = QColor(0, 255, 0)
 		self.red = "background-color: rgb(255,92,92);\n"
-		Red = QColor(255, 54, 14)
-
 		Value = caget(self.Test_PV)
 		if Value == 1.0:
-			self.ui.CavityStatus.setStyleSheet( str(self.green))
-			self.ui.CavityStatus.brush.setColor(Green)
+			self.ui.CavityStatusBar.setStyleSheet( str(self.green))
 		else:
-			#self.ui.CavityStatus.setStyleSheet( str(self.red) )
-			self.ui.CavityStatus.brush.setColor(Red)	
-			
+			self.ui.CavityStatusBar.setStyleSheet( str(self.red) )
 
 		
 		

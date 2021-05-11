6420e9f3e4f86221dd8359d936f0513e86c08842
dabisi <dabisi@slac.stanford.edu>
2021-05-11 10:07:24 -0700

    Learning how to use a template repeater

---
+++ Derikka/cryomoduleTemplateRepeater.py
@@ -0,0 +1,22 @@
+from pydm import Display
+from PyQt5 import QtGui, QtCore, QtWidgets 
+from PyQt5.QtGui import QPainter, QColor, QBrush, QPen 
+from PyQt5.QtCore import Qt, QObject
+import epics
+from epics import caget, caput
+from PyQt5.QtWidgets import (QWidgetItem, QCheckBox, QPushButton, QLineEdit,
+                             QGroupBox, QHBoxLayout, QMessageBox, QWidget,
+                             QLabel, QFrame, QComboBox)
+from pydm.widgets import PyDMDrawingRectangle, PyDMLabel
+from pydm.widgets.drawing import PyDMDrawingPolygon
+
+class cryomoduleTemplateRepeater(Display):
+
+	def ui_filename(self):
+		return 'cryomoduleTemplateRepeater.ui'
+		
+	def __init__(self, parent = None, args = None):
+		super(cryomoduleTemplateRepeater, self).__init__(parent=parent,args=args)
+		
+		self.ui.cmTemplate.loadWhenShown = False
+		 

45f445291aaa3ebc5796ccb8cba0f6eb777ce3d1
dabisi <dabisi@slac.stanford.edu>
2021-05-11 13:43:04 -0700

    Read in cavity PyDMLabels and change shape color

--- Derikka/cryomoduleTemplateRepeater.py
+++ Derikka/cryomoduleTemplateRepeater.py
@@ -20,3 +20,56 @@ class cryomoduleTemplateRepeater(Display):
 		
 		self.ui.cmTemplate.loadWhenShown = False
 		
+		# --- Read in drawing object and labels from EmbeddedSingleCavity.ui ---
+		shapeList = self.ui.cmTemplate.findChildren(PyDMDrawingPolygon)
+		
+		# Make seperate lists for cavity numbers and TLC
+		cavityNumberList = []
+		cavityTLCList = []
+			
+		labelList = self.ui.cmTemplate.findChildren(PyDMLabel)
+		for index, items in enumerate (labelList):
+			if index%2 == 0:
+				cavityNumberList.append(items)
+			else:
+				cavityTLCList.append(items)
+		
+		
+		# Practice changing a cavity shape color
+		self.makeItRed(shapeList[0])
+		self.makeItYellow(shapeList[2])
+		self.makeItGreen(shapeList[4])
+
+# Change shape color to yellow warning
+	def makeItYellow(self, shape):
+		yellow = QColor(255,253,167)
+		neonYellow = QColor(248,228,0)
+		
+		shape.brush.setColor(yellow)
+		shape.penColor = neonYellow
+		shape.numberOfPoints = 4
+		shape.rotation = 0
+		shape.update()
+		
+# Change shape color to red warning
+	def makeItRed(self, shape):
+		red = QColor(255,195,187)
+		neonRed = QColor(255,0,0)
+		
+		shape.brush.setColor(red)
+		shape.penColor = neonRed
+		shape.numberOfPoints = 6
+		shape.rotation = 0
+		shape.update()
+
+# Change shape color to green
+	def makeItGreen(self, shape):
+		green = QColor(201,255,203)
+		neonGreen = QColor(46,248,10)
+		
+		shape.brush.setColor(green)
+		shape.penColor = neonGreen
+		shape.numberOfPoints = 4
+		shape.rotation = 0
+		shape.update()
+

2b91bc43335120f889dcfa4517ae8efba289c81b
dabisi <dabisi@slac.stanford.edu>
2021-08-10 12:18:38 -0700

    Using pandas to read csv data to Fault class

--- fault.py
+++ fault.py
@@ -1,10 +1,13 @@
 from csv import reader
+import pandas as pd
+
 
 class Fault:
-    def __init__(self, rowData, rank):
-        self.severity = int(rowData[8])
+    def __init__(self, tlc, severity, rank):
+        self.tlc = tlc
+        self.severity = severity
         self.rank = rank
-        self.tlc = rowData[7]
+        print(tlc, severity, rank)
 
     def __gt__(self, other):
         return self.rank > other.rank
@@ -17,14 +20,26 @@ class Fault:
     def writeToPVs(self):
         # if faulted, write tlc to CUDSTATUS pv and severity to CUDSEVR pv
         pass
-
+'''
 csvRowList = []
 faults = []
 csvReader = reader(open("faults.csv"))
 header = next(csvReader)
+print(header)
 
 for row in csvReader:
-    print(row)
+    # print(row)
     csvRowList.append(row)
-    faults.append(Fault(row,csvReader.line_num))
+    #faults.append(Fault(row,csvReader.line_num))
+'''
+
+dataFrame = pd.read_csv('faults.csv')
+pd.set_option('display.max_colwidth', -1)
+
+for tlc, colorSeverity, rank in zip(dataFrame['TLC'][1:], dataFrame['Severity'][1:], dataFrame['Order'][1:]):
+    Fault(tlc, colorSeverity, rank)
+
+
+
+
 

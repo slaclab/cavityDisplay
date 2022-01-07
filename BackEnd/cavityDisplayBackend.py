ff8c89fc3194af0eeee5422bdecc4ca8f38ca732
dabisi <dabisi@slac.stanford.edu>
2022-01-07 15:23:52 -0800

    Changed how we clear Lists in backend code to be more efficient

--- BackEnd/cavityDisplayBackend.py
+++ BackEnd/cavityDisplayBackend.py
@@ -11,23 +11,30 @@ class DisplayValues:
         self.severityValues = []
 
     def clearLists(self):
-        self.statusNames = []
-        self.statusValues = []
-        self.severityNames = []
-        self.severityValues = []
-
+        self.statusNames*=0
+        self.statusValues*=0
+        self.severityNames*=0
+        self.severityValues*=0
+        
 
 displayValues = DisplayValues()
 
-while True:
-    start = time()
-    for linac in DISPLAY_LINACS:
-        for cryomodule in linac.cryomodules.values():
-            for cavity in cryomodule.cavities.values():
-                cavity.runThroughFaults(displayValues)
 
-    caput_many(displayValues.statusNames + displayValues.severityNames,
-               displayValues.statusValues + displayValues.severityValues)
-    displayValues.clearLists()
-    
-    print(time() - start)
+try:
+    while True:
+        start = time()
+        for linac in DISPLAY_LINACS:
+            for cryomodule in linac.cryomodules.values():
+                for cavity in cryomodule.cavities.values():
+                    cavity.runThroughFaults(displayValues)
+
+        caput_many(displayValues.statusNames + displayValues.severityNames,
+                displayValues.statusValues + displayValues.severityValues)
+
+        displayValues.clearLists()
+
+        print(time() - start)
+        
+except KeyboardInterrupt:
+    print('Interuption! Program will end within the next minute (aka please be patient)')
+    pass

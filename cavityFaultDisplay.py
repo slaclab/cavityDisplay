706ff3a84d802d7f275bdb56d9c44ae4dd2dff35
Gabriella Lopardo <lopardog@slac.stanford.edu>
2022-03-24 00:07:40 -0700

    added a class in cavityFaultDisplay.py to populate the 3letterfaults ui file with fault descriptions

--- cavityFaultDisplay.py
+++ cavityFaultDisplay.py
@@ -46,6 +46,43 @@ class CavityFaultDisplay(Display):
 
             fault.pv.add_callback(partial(self.statusLabelCallback, statusLabel, fault))
 
+
+class ThreeLetterFaultDisplay(Display):
+    def __init__(self, parent=None, args=None, macros=None):
+        super().__init__(parent=parent, args=args,
+                         ui_filename="frontend/3letterfaults.ui",
+                         macros=macros)
+
+        linacIdx = int(macros["linac"][1])
+        cryomoduleName = macros["cryoNum"]
+        cavityNumber = macros["cavityNumber"]
+
+        cavityObject = DISPLAY_LINAC_OBJECTS[linacIdx].cryomodules[cryomoduleName].cavities[cavityNumber]
+
+        faults: List[Fault] = cavityObject.faults
+        verticalLayout: QVBoxLayout = self.ui.layout
+
+        for fault in faults:
+            horizontalLayout = QHBoxLayout()
+            descriptionLabel = QLabel()
+            descriptionLabel.setText(fault.description)
+            descriptionLabel.setSizePolicy(QSizePolicy.MinimumExpanding,
+                                           QSizePolicy.MinimumExpanding)
+
+            codeLabel = QLabel()
+            codeLabel.setText(fault.tlc)
+            codeLabel.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+
+            nameLabel = QLabel()
+            nameLabel.setText(fault.name)
+            nameLabel.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+
+            horizontalLayout.addWidget(codeLabel)
+            horizontalLayout.addWidget(nameLabel)
+            horizontalLayout.addWidget(descriptionLabel)
+
+            verticalLayout.addLayout(horizontalLayout)
+
     @staticmethod
     def statusLabelCallback(label: QLabel, fault: Fault, **kw):
         try:

906b4abad609d20ddf9b5deaa9963c20c00d3c3e
Gabriella Lopardo <lopardog@slac.stanford.edu>
2022-03-25 21:45:41 -0700

    temporarily took out TLC button on cryomodule panel until it's fixed

--- 3letterfault.py
+++ 3letterfault.py
@@ -22,12 +22,39 @@ class ThreeLetterFaultDisplay(Display):
         faults: List[Fault] = cavityObject.faults
         verticalLayout: QVBoxLayout = self.ui.tlclayout
 
+        headerLayout = QHBoxLayout()
+        descriptionheaderLabel = QLabel()
+        descriptionheaderLabel.setText("Description")
+        descriptionheaderLabel.setMinimumSize(200, 30)
+        descriptionheaderLabel.setStyleSheet("text-decoration: underline")
+
+        nameheaderLabel = QLabel()
+        nameheaderLabel.setText("Name")
+        nameheaderLabel.setMinimumSize(200, 30)
+        nameheaderLabel.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum)
+        nameheaderLabel.setStyleSheet("text-decoration: underline")
+
+        codeheaderLabel = QLabel()
+        codeheaderLabel.setText("Code")
+        codeheaderLabel.setMinimumSize(30, 30)
+        codeheaderLabel.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        codeheaderLabel.setStyleSheet("text-decoration: underline")
+
+        headerLayout.setAlignment(Qt.AlignVCenter | Qt.AlignLeft)
+
+        headerLayout.addWidget(codeheaderLabel)
+        headerLayout.addWidget(nameheaderLabel)
+        headerLayout.addWidget(descriptionheaderLabel)
+        headerLayout.setSpacing(50)
+
         for fault in faults.values():
             horizontalLayout = QHBoxLayout()
             descriptionLabel = QLabel()
             descriptionLabel.setText(fault.description)
-            descriptionLabel.setSizePolicy(QSizePolicy.MinimumExpanding,
-                                           QSizePolicy.MinimumExpanding)
+            descriptionLabel.setMinimumSize(300, 30)
+            descriptionLabel.setSizePolicy(QSizePolicy.Minimum,
+                                           QSizePolicy.Minimum)
+            descriptionLabel.setWordWrap(True)
 
             codeLabel = QLabel()
             codeLabel.setText(fault.tlc)
@@ -35,10 +62,15 @@ class ThreeLetterFaultDisplay(Display):
 
             nameLabel = QLabel()
             nameLabel.setText(fault.name)
-            nameLabel.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+            nameLabel.setMinimumSize(200, 30)
+            nameLabel.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum)
+            nameLabel.setWordWrap(True)
 
             horizontalLayout.addWidget(codeLabel)
             horizontalLayout.addWidget(nameLabel)
             horizontalLayout.addWidget(descriptionLabel)
 
+            horizontalLayout.setSpacing(50)
+            horizontalLayout.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
             verticalLayout.addLayout(horizontalLayout)
+        verticalLayout.setSpacing(10)

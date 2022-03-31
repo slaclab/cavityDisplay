4439c2e98229fa0f63cef723ef93018009309b59
Lisa Zacarias <zacarias@slac.stanford.edu>
2022-03-30 18:34:23 -0700

    replacing time printout with watcher heartbeat increment

--- cavityDisplayBackend.py
+++ cavityDisplayBackend.py
@@ -1,9 +1,9 @@
-# from epics import PV
+from epics import PV
 from time import time
 
 from displayCavity import DISPLAY_LINAC_OBJECTS
 
-# WATCHER_PV = PV("PHYS:SYS0:1:SC_CAV_FAULT_HEARTBEAT")
+WATCHER_PV = PV("PHYS:SYS0:1:SC_CAV_FAULT_HEARTBEAT")
 
 while True:
     start = time()
@@ -12,5 +12,4 @@ while True:
             for cavity in cryomodule.cavities.values():
                 cavity.runThroughFaults()
 
-    # WATCHER_PV.put(WATCHER_PV.value + 1)
-    print(time() - start)
+    WATCHER_PV.put(WATCHER_PV.value + 1)

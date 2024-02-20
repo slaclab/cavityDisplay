705ffbdffa951f34e41fcb9fb23f453ab2fb4fa6
Lisa Zacarias <zacarias@slac.stanford.edu>
2024-02-20 15:20:00 -0800

    adding SSA 480 fault

--- utils.py
+++ utils.py
@@ -11,11 +11,19 @@ DEBUG = False
 BACKEND_SLEEP_TIME = 10
 
 CSV_FAULTS: List[Dict] = []
-for row in DictReader(open("faults.csv", encoding='utf-8-sig')):
+for row in DictReader(open("faults.csv", encoding="utf-8-sig")):
     if row["PV Suffix"]:
         CSV_FAULTS.append(row)
 
 
-def displayHash(rack: str, faultCondition: str, okCondition: str, tlc: str, suffix: str,
-                prefix: str):
-    return hash(rack) ^ hash(faultCondition) ^ hash(okCondition) ^ hash(tlc) ^ hash(suffix) ^ hash(prefix)
+def displayHash(
+    rack: str, faultCondition: str, okCondition: str, tlc: str, suffix: str, prefix: str
+):
+    return (
+        hash(rack)
+        ^ hash(faultCondition)
+        ^ hash(okCondition)
+        ^ hash(tlc)
+        ^ hash(suffix)
+        ^ hash(prefix)
+    )

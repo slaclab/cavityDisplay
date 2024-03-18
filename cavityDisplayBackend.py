d57380c8a9fd6a945d56a186a537c1b29526cfda
Lisa Zacarias <zacarias@slac.stanford.edu>
2024-03-18 12:15:49 -0700

    bringing in line with recent changes to lcls tools

--- cavityDisplayBackend.py
+++ cavityDisplayBackend.py
@@ -4,7 +4,7 @@ from time import sleep
 from lcls_tools.common.controls.pyepics.utils import PV
 from lcls_tools.superconducting.sc_linac_utils import ALL_CRYOMODULES
 
-from displayLinac import DISPLAY_CRYOMODULES, DisplayCryomodule
+from displayLinac import DISPLAY_MACHINE, DisplayCryomodule
 from utils import BACKEND_SLEEP_TIME, DEBUG
 
 WATCHER_PV: PV = PV("PHYS:SYS0:1:SC_CAV_FAULT_HEARTBEAT")
@@ -13,7 +13,7 @@ WATCHER_PV.put(0)
 while True:
     start = datetime.now()
     for cryomoduleName in ALL_CRYOMODULES:
-        cryomodule: DisplayCryomodule = DISPLAY_CRYOMODULES[cryomoduleName]
+        cryomodule: DisplayCryomodule = DISPLAY_MACHINE.cryomodules[cryomoduleName]
         for cavity in cryomodule.cavities.values():
             cavity.run_through_faults()
     if DEBUG:
